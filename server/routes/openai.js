3b860a8dee9065eab7d4f8c0f6a88241bdea2e11
Stephen <stephen.a.nerby@gmail.com>
2023-07-28 14:45:32 -0400

    Openai Post route Client & Server, Redux store made

--- server/routes/openai.js
+++ server/routes/openai.js
@@ -9,6 +9,7 @@ const router = express.router()
 router.post("/text", async (req, res) => {
     try {
         const { text, activeChatId } = req.body;
+        res.status(200).json({ text })
     } catch (error) {
         console.error("Error", error)
         res.status(500).json({ error: error.message })
